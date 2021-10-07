39df43b85b949e7cc99ee9084d939ac9eff3721b
natasha_n <nbnulton@gmail.com>
2021-10-07 17:48:26 -0400

    first commit

---
+++ homework_mod.py
@@ -0,0 +1,12 @@
+def square_footage(length,width):
+    print(f"length: {length}, width: {width}")
+    length = int(length)
+    width = int(width)
+    area = length * width
+    print(f"Your square footage of your house is: ", area)
+    
+def circumference(diameter):
+    print(f"diameter: {diameter}")
+    diameter = int(diameter)
+    circumference = diameter | 3.14
+    print(f"Your circumference of your circle is: ", circumference)
---
+++ my_mod.py
@@ -0,0 +1,5 @@
+def say_hello(name):
+    print(f"hello {name}")
+    
+def say_goodbye(name):
+    print(f"goodbye {name}")
